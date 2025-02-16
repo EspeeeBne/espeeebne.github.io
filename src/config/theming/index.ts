5242f1a1689e133f494c80d72e383b86de0c5bef
Espe <185096668+EspeeeBne@users.noreply.github.com>
2025-02-16 12:01:19 +0300

    git commit bir gün düzgün çalışsa şaşarım

--- src/config/theming/index.ts
+++
@@ -1,6 +0,0 @@
-import darkTheme from './dark/index';
-import lightTheme from './light/index';
-
-export const getTheme = (mode: 'light' | 'dark') => (mode === 'light' ? lightTheme : darkTheme);
-
-export { darkTheme, lightTheme };
