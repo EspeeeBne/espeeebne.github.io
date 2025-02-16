3f85b7549f1fff87e224473d79fd63ecf62cbd75
Espe <185096668+EspeeeBne@users.noreply.github.com>
2025-02-16 07:59:47 +0300

    fixes #2 really

---
+++ src/pages/api/sitemap.xml.tsx
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import projectsData from '../../../src/data/projectsData';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const baseUrl = 'https://espeeebne.github.io';
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${baseUrl}</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>1.0</priority>
+     </url>
+     <url>
+       <loc>${baseUrl}/about</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>weekly</changefreq>
+       <priority>0.8</priority>
+     </url>
+     <url>
+       <loc>${baseUrl}/projects</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>weekly</changefreq>
+       <priority>0.8</priority>
+     </url>
+     ${projectsData
+       .map((project) => {
+         return `
+       <url>
+         <loc>${baseUrl}/projects/${project.slug}</loc>
+         <lastmod>${new Date(project.date).toISOString()}</lastmod>
+         <changefreq>weekly</changefreq>
+         <priority>0.7</priority>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+}
