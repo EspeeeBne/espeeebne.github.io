c473202b9565e760cdb4c488849bbd9cb5eba6b9
Espe <185096668+EspeeeBne@users.noreply.github.com>
2025-06-17 05:13:25 +0300

    typescript çok şartlı çalışıyoooooooo!!!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,4 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentProps } from "next/document";
-
-interface MyDocumentProps extends DocumentProps {
-    pathname: string;
-}
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -13,39 +9,10 @@ class MyDocument extends Document {
     }
 
     render() {
-        const { pathname } = this.props as MyDocumentProps;
-
-        const getMetaImage = () => {
-            const match = pathname.match(/^\/projects\/([^/]+)\/?$/);
-            if (match) {
-                const slug = match[1];
-                return `https://espeeebne.github.io/static/projects/${slug}/metadata.png`;
-            }
-            return "https://espeeebne.github.io/static/important-images/favicon.ico";
-        };
-
-        const metaImage = getMetaImage();
 
         return (
             <Html lang="tr">
                 <Head>
-                    // burası statik çalışıyor components içindeki dinamik ondan burası not oldu
-                    /*<meta name="description" content="Espe Portfolio - Kendim için yaptığım portfolyo sitesi." />
-                    <meta property="og:title" content="Espe Portfolio" />
-                    <meta
-                        property="og:description"
-                        content="Espe Portfolio - Kendim için yaptığım portfolyo sitesi."
-                    />
-                    <meta property="og:image" content={metaImage} />
-                    <meta property="og:url" content={`https://espeeebne.github.io${pathname}`} />
-                    <meta name="twitter:card" content={metaImage} />
-                    <meta name="twitter:title" content="Espe Portfolio" />
-                    <meta
-                        name="twitter:description"
-                        content="Espe Portfolio - Kendim için yaptığım portfolyo sitesi."
-                    />
-                    <meta name="twitter:image" content={metaImage} />
-                    <meta charSet="utf-8" />*/
                     <link rel="icon" href="/static/important-images/favicon.ico" />
                 </Head>
                 <body>
