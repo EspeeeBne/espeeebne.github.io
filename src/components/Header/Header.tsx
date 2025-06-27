d59301f236888cdc13d22b98a8fb6f6318c24de8
EspeeeBne <185096668+EspeeeBne@users.noreply.github.com>
2025-06-27 18:51:45 +0300

    bota guven olmazmıs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -75,8 +75,8 @@ const modalVariants = {
 };
 
 const sidebarVariants = {
-    open: { x: 0, transition: { type: 'spring', stiffness: 300, damping: 30 } },
-    closed: { x: '-100%', transition: { type: 'spring', stiffness: 300, damping: 30 } },
+    open: { x: 0, transition: { type: "spring" as const, stiffness: 300, damping: 30 } },
+    closed: { x: '-100%', transition: { type: "spring" as const, stiffness: 300, damping: 30 } },
 };
 
 const listItemVariants = {
